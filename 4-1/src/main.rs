0b088e2fe9bde03eb664f99ed5ffbcd636687d92
DrPandemic <bipbip500@gmail.com>
2017-12-04 22:33:45 -0500

    Day 4 - 1

--- 4-1/src/main.rs
+++ 4-1/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Result;
+
+fn read_file() -> Result<Vec<Vec<String>>> {
+  let mut file = File::open("input")?;
+  let mut content = String::new();
+  file.read_to_string(&mut content)?;
+
+  let lines = content.split('\n').map(|s| s.to_string()).filter(|s| s != "");
+  Ok(lines.map(|line| line.split(' ').map(str::to_string).collect()).collect())
+}
+
+fn filter_passwords(input: Vec<Vec<String>>) -> Vec<Vec<String>> {
+  input.into_iter().filter(|line| {
+    let mut cleaned = line.clone();
+    cleaned.sort();
+    cleaned.dedup();
+
+    println!("{:?} {:?}\n", line, cleaned);
+
+    cleaned.len() == line.len()
+  }).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("{:?}", filter_passwords(read_file().unwrap()).len());
 }
