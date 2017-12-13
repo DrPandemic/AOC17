29aa797a2be1567979f0a33002a3757a9d470c74
DrPandemic <bipbip500@gmail.com>
2017-12-12 21:59:50 -0500

    Day 7 - 2

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -40,14 +40,47 @@ fn get_node<'a>(data: &'a Container, name: &String) -> &'a Node {
   data.iter().find(|n| &n.name == name).unwrap()
 }
 
-fn total_weight(data: &Container, name: &String) -> i32 {
-  let node: &Node = get_node(data, name);
-  let weight: i32 = node.children.iter().map(|c| total_weight(data, c)).sum();
+fn total_weight(data: &Container, node: &Node) -> i32 {
+  let weight: i32 = node.children.iter().map(|c| total_weight(data, get_node(data, c))).sum();
   node.weight + weight
+}
+
+fn find_unbalanced(data: &Container) {
+  for node in data {
+    let children: Vec<(&Node, i32)> = node.children.iter().map(|c| {
+      let curr = get_node(data, c);
+      (curr, total_weight(data, curr))
+    }).collect();
+    let min = children.iter().map(|e| e.1).min();
+    let max = children.iter().map(|e| e.1).max();
+    if min != max {
+      let mut list: Vec<Vec<(&Node, i32)>> = vec![];
+
+      for (child, weight) in children {
+        // I don't like this
+        let mut should_push = false;
+        {
+          let sub_list = list.iter_mut().find(|e| e.iter().any(|&(_, other_weight)| weight == other_weight));
+          should_push = match sub_list {
+            None => true,
+            Some(the_list) => {
+              the_list.push((child, weight));
+              false
+            },
+          };
+        }
+
+        if should_push {
+          list.push(vec![(child, weight)]);
+        }
+      }
+
+      println!("{:?}", list);
+    }
+  }
 }
 
 fn main() {
   let data = read_file().unwrap();
-  println!("{:?}", data.iter().max_by(|a, b| total_weight(&data, &a.name).cmp(&total_weight(&data, &b.name))));
-  println!("Hello, world!");
+  find_unbalanced(&data);
 }
